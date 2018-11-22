25173c81c4228c4d6cd7617edaedb9b0fe074fe1
Cendekia Pramana Putra <me@cendekiapp.com>
2018-11-22 15:04:38 +0700

    human validation bot

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use BotMan\BotMan\BotMan;
 use Illuminate\Http\Request;
+use BotMan\BotMan\Middleware\Dialogflow;
 use App\Conversations\ExampleConversation;
 
 class BotManController extends Controller
@@ -13,7 +14,30 @@ class BotManController extends Controller
      */
     public function handle()
     {
+        $dialogflow = Dialogflow::create(env('DIALOGFLOW_TOKEN'))->listenForAction();
+
         $botman = app('botman');
+        $botman->middleware->received($dialogflow);
+
+        $botman->hears('btt:.*', function (BotMan $bot) {
+            $extras = $bot->getMessage()->getExtras();
+            $user = $bot->getUser();
+
+            // mockup user data
+            $bot->userStorage()->save([
+                'id' => $user->getId(),
+                'first_name' => $user->getFirstName(),
+                'last_name' => $user->getLastName(),
+                'username' => $user->getUsername(),
+                'user_info' => [],
+            ]);
+
+            \Log::debug($extras);
+            foreach ($extras['apiTextResponses'] as $key => $data) {
+                $bot->typesAndWaits(rand(1,2));
+                $bot->reply($data['speech']);   
+            }
+        })->middleware($dialogflow);
 
         $botman->listen();
     }
